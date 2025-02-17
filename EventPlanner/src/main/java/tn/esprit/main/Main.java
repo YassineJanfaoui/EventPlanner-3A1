99559b3b86d307cfa9d059eebb539e010647300a
YassineJanfaoui <janfaouimohamed16@gmail.com>
2025-02-17 21:32:57 +0100

    removed folder from main

--- EventPlanner/src/main/java/tn/esprit/main/Main.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.main;
-
-import tn.esprit.utils.MyDatabase;
-
-public class Main {
-    public static void main(String[] args) {
-        MyDatabase db = MyDatabase.getInstance();
-    }
-}
